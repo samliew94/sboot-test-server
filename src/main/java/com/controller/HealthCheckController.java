c2112699e0dc90799b7e9bf7a8d0c2b5ce918346
pcccmn <mlmsamliew@gmail.com>
2023-06-22 01:15:04 +0800

    added dockerfile

---
+++ src/main/java/com/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("health")
+public class HealthCheckController {
+
+	@GetMapping("check")
+	public ResponseEntity healthCheck() {
+		return ResponseEntity.ok().build();
+	}
+	
+}
